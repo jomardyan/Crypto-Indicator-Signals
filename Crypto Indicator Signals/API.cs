10bd85faa3e4a10b2f7f65d05401ea5f3e24c69f
Hayk Jomardyan <jomardyan@outlook.com>
2020-07-02 21:22:49 +0200

    Add project files.

---
+++ Crypto Indicator Signals/API.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto_Indicator_Signals
+{
+    class API
+    {
+        public int state { get; set; }
+        #region HTTP GLOBAL ERRORS
+        string HTTP_403 = "WAF Limit (Web Application Firewall) has been violated";
+        string HTTP_429 = "Breaking a request rate limit";
+        string HTTP_418 = "IP Address has been banned";
+        string HTTP_4XX = "";
+        string HHTP_5XX = ""; 
+        #endregion
+
+        string BASE_URL = "https://api.binance.com"; 
+        public API()
+        {
+
+        }
+    }
+}
